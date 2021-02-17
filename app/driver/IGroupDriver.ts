4af06e0e96b13874494ce00af7360ee2f22d8410
Le Hoang Long <le.hoang.long@outlook.com>
2021-02-17 13:54:42 +0800

    step 1 of creating a submodule

--- app/driver/IGroupDriver.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../model/User';
-
-interface IGroupDriver {
-    getUsers() : User[];
-    addUser(user: User): void;
-    save(): void;
-}
--- app/types.ts
+++
@@ -1,9 +0,0 @@
-export const TYPES = {
-    UserDriver: Symbol.for("UserDriver"),
-    UserDatabaseClientPool: Symbol.for("UserDatabaseClientPool"),
-    UserController: Symbol.for("UserController"),
-    UserView: Symbol.for("UserView"),
-    JwtSecretKey: Symbol.for("JwtSecretKey"),
-    JwtDuration: Symbol.for("JwtDuration"),
-    JwtAuthentication: Symbol.for("JwtAuthentication"),
-};
